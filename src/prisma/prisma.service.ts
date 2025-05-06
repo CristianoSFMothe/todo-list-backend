9a0de3b1b5d02b04ab83a78f8555f2f16ef1526a
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-06 17:57:15 -0300

    feat: add Prisma setup with user model and database configuration

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
