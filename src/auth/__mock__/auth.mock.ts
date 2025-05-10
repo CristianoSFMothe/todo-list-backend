4e65281a92efe97433fbe7899efa6b6d9c4851c2
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-10 16:34:37 -0300

    feat: refactor AuthService tests and remove unused files

---
+++ src/auth/__mock__/auth.mock.ts
@@ -0,0 +1,12 @@
+export const mockUser = {
+  id: 'user-id',
+  email: 'test@email.com',
+  password: 'hashed-password',
+};
+
+export const mockJwtToken = 'mock-jwt-token';
+
+export const mockAuthEntity = {
+  token: mockJwtToken,
+  email: mockUser.email,
+};
