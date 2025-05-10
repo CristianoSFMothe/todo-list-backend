4ccaf2553d4b62cda1616cb3accaec31a0a6d6d1
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-10 09:38:11 -0300

    feat: add createTask method tests to TaskController with success and error scenarios

--- src/task/__tests__/task.controller.spec.ts
+++ src/task/__tests__/task.controller.spec.ts
@@ -1,18 +1,59 @@
+// src/task/task.controller.spec.ts
+
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { TaskController } from '../task.controller';
+import { TaskService } from '../task.service';
+import { createTaskDtoMock, taskEntityMock } from '../__mock__/task.mock';
 
 describe('TaskController', () => {
   let controller: TaskController;
+  let service: TaskService;
+
+  const mockTaskService = {
+    createTask: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
-    }).compile();
+      providers: [
+        {
+          provide: TaskService,
+          useValue: mockTaskService,
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: () => true }) // Ignora o guard no teste
+      .compile();
 
     controller = module.get<TaskController>(TaskController);
+    service = module.get<TaskService>(TaskService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createTask', () => {
+    it('should create a task successfully', async () => {
+      mockTaskService.createTask.mockResolvedValue(taskEntityMock);
+
+      const result = await controller.createTask(createTaskDtoMock);
+
+      expect(result).toEqual(taskEntityMock);
+      expect(service.createTask).toHaveBeenCalledWith(createTaskDtoMock);
+    });
+
+    it('should throw ConflictException if task title already exists', async () => {
+      const conflictError = new Error('Conflict');
+      mockTaskService.createTask.mockRejectedValue(conflictError);
+
+      await expect(controller.createTask(createTaskDtoMock)).rejects.toThrow(
+        conflictError,
+      );
+      expect(service.createTask).toHaveBeenCalledWith(createTaskDtoMock);
+    });
   });
 });
